323c5607507f4d3793c77a1f4604f665d2df08f8
Julien <verdun.julien@yahoo.fr>
2020-09-03 16:24:19 +0200

    Update time simulation

--- configuration.py
+++ configuration.py
@@ -1,3 +1,4 @@
 target_companies = ["MSFT", "DAX"]
 simulation_time = 5
 timelapse = 1
+simulation_date = "01-01-2010"
--- main.py
+++ main.py
@@ -1,30 +1,27 @@
 """
-In the main file, the class are initialized and the bot 
-checks the market at regular time interval and buy and sell 
+In the main file, the class are initialized and the bot
+checks the market at regular time interval and buy and sell
 stocks in order to make the maximum of money.
 """
 
 import time
 from configuration import *
-# from bot import Bot
+#from bot import Bot
 
 
-#bot = Bot(target_companies)
-
-# initialisation du temps
-
-#t0 = datetime.timestamp(datetime(2015, 1, 1)) + 24 * 3600 * 1000
-
-t0 = time.strptime("01-01-2010", "%d-%m-%Y")
+# time initialisation
+t0 = time.strptime(simulation_date, "%d-%m-%Y")
 t0 = time.mktime(t0)
 i = 0
+
+# box initialisation
+# bot = Bot(target_companies,)
+
+
 # toutes les timestep secondes
 while i < simulation_time:
     t0 += 24 * 3600
-    print("go : ", time.gmtime(t0))
-
+    print("go : ", time.strftime("%d-%m-%Y", time.gmtime(t0)))
+    # bot.run(strftime("%d-%m-%Y", time.gmtime(t0))
     time.sleep(timelapse)
     i += 1
-
-
-# partir d'une date fixe, la passer en argument du bot etc, et incrementer le jour
